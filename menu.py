84b66fca1bc4a83160ec9b86548786e62548a4b0
keithmmc <100876230+keithmmc@users.noreply.github.com>
2024-03-24 16:23:01 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+
 def main():
     displayMenu()
     while True:
@@ -7,13 +8,47 @@ def main():
         mysql_connect.viewCitiesByCountry()
         displayMenu()
     
+    elif (choice == 2):
+        print("View Countries by Independence Year")
+        while True:
+            try:
+                year = int(input("Please Enter the Year you wish to query: "))
+                break 
+            except ValueError:
+                 print("Invalid year entered. Please enter a valid number for the year")
+            countries = mysql_connect.viewCountriesByIndependenceYear(year)
+            for countries in countries:
+                print(country['Name'], ":", country['Continent'], ":", country["IndepYear"])
+            displayMenu()
+    
+    elif (choice == 3):
+        print("Add a person to the database")
+        person_name = input("personname: ")
+        age = input("Age: ")
+        salary = input("Salary: ")
+        city = input("city:")
+        results = mysql_connect.addNewPerson(person_name, age, salary, city)
+        print("The database has been updated would you like to View people")
+        if choice == "yes":
+            mysql_connect.viewPeopleByPerson()
+        if choice == "no":
+            displayMenu()
+            
+    elif (choice == 4):
+        country_name = str(input("Please enter the Country Name: "))
+        countries = mysql.connect.getCountryData()
+        for country in countries:
+             if str.casefold(country_name) in str.casefold(country["Name"]):
+                    print(country["Name"],":",country["Continent"],":",country["Population"],":",country["HeadOfState"])
+             displayMenu()
+            
+            
     
 
     elif (choice == "x"):
         print("goodbye thanks for using our database")
 
-    else: 
-            
+    else:  
         displayMenu()
     
 
@@ -27,8 +62,8 @@ def displayMenu():
     print("3 - Add New Person")
     print("4 - View Countries By name")
     print("5 - View Countries By Population")
-    print("6 - Find Students By Address")
-    print("7 - Add New Course")
+    print("6 - View Twinned Cities")
+    print("7 - Twin with Dublin")
     print("x - To Exit The Database")
     print("------------------------")
 
